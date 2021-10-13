b3a4a7b90b7bb203019fcc3f0549fb15d8c1552d
Mihir Waykole <mihirwaykole1703@yahoo.com>
2021-10-13 12:23:36 +0530

    multiMessage

--- emailBot.py
+++ emailBot.py
@@ -1,14 +1,17 @@
 import json
 import smtplib
-
-def getEmailInfo():
-    return json.load(open('email-config.json'))
-def getSenderInfo():
-    return json.load(open('./Send/email.json'))
-
 server = smtplib.SMTP('smtp.gmail.com',587)
 server.starttls()
-info = getEmailInfo()
-sender = getSenderInfo()
-server.login(info['mail'],info['password']) 
-server.sendmail(info['mail'],sender['mail'],sender['message'])
+
+def getEmailInfo():
+    return json.load(open('./email-config.json'))
+def getSenderInfo():
+    return json.load(open('./Send/email.json'))
+def sendMail():
+    info = getEmailInfo()
+    sender = getSenderInfo()
+    server.login(info['mail'],info['password']) 
+    server.sendmail(info['mail'],sender['mail'],sender['message'])
+
+for i in range (20):
+    sendMail()
